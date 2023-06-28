06d544673107259d7b655096d2dd44ba12ba333f
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2023-06-28 15:40:57 +0000

    update config for test database

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 const PORT = process.env.PORT;
-const DATABASE_URL = process.env.DATABASE_URL;
+
+const DATABASE_URL = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_DATABASE_URL
+  : process.env.DATABASE_URL
 
 module.exports = {
   PORT,
