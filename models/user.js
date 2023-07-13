05706f77b6cf263a71ee4127e1f643cd7eb1bd82
Adam Ascencio <52258246+adamascencio@users.noreply.github.com>
2023-07-13 15:16:41 +0000

    install package to require user-generated usernames to be unique

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const { Schema, model } = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator')
 
 const userSchema = new Schema({
-    username: String,
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
     name: String,
     passwordHash: String,
     notes: [
@@ -12,6 +17,7 @@ const userSchema = new Schema({
     ]
 });
 
+userSchema.plugin(uniqueValidator);
 userSchema.set('toJSON', {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString();
